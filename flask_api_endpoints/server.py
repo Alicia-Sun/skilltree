a76a46b68d3d73446addf6e95a0a82148b76a97c
Alicia <aliciasun0102@gmail.com>
2023-09-09 12:23:50 -0400

    new endpoints

--- flask_api_endpoints/server.py
+++ flask_api_endpoints/server.py
@@ -1,14 +1,16 @@
-from flask import Flask, request, jsonify
+from typing import Any
+from flask import Flask, request, jsonify, make_response
 from pymongo import MongoClient, InsertOne
 import json
+import uuid
 
 
 app = Flask(__name__)
 
 
 
-client = MongoClient("mongodb://localhost:27017")
-myDB = client['SkillTreeDB']
+client = MongoClient("mongodb+srv://skilltree:skilltree@skilltree.mm3cfn3.mongodb.net/?retryWrites=true&w=majority")
+db = client['SkillTreeDB']
 
 
 
@@ -20,45 +22,68 @@ myDB = client['SkillTreeDB']
 
 @app.route("/generate", methods=["POST"])
 def generate():
-    pass
+    content = request.json
+    db["trees"].insertOne(generateTree(content['topic'], content['skills']))         #Kenny's
+    return {"sucess": True}
+
 
 @app.route("/login", methods=["POST"])
 def login():
-    pass
+    content = request.json
+    users = db["users"]
+    sessions = db["sessions"]
+
+    user = users.find_one({"phone": content["phone"]})
+    if user is None:
+        return {"success": False}
+
+    if user["password"] != content["password"]:
+        return {"success": False}
+
+    sessid = uuid.uuid4()
+    session = sessions.insert_one({"user_id": user["_id"], "id": str(sessid)})
+    resp = make_response({"success": True})
+    resp.set_cookie("session", str(sessid))
+
+    return resp
 
 @app.route("/logout", methods=["POST"])
 def logout():
-    pass
+    content = request.json
+    sessions = db["sessions"]
+    session = sessions.find_one({"id": content["session"]})
 
-@app.route("/path/<int:id>", methods=["GET", "POST"])
-def path():
-    pass
+    if session is None:
+        resp = make_response({"success": False})
+    else:
+        sessions.delete_one({"id": content["session"]})
+        resp = make_response({"success": True})
+
+    resp.set_cookie("session", "", expires=0)
+    return resp
+
+@app.route("/path/<int:id>", methods=["GET"])       #removed "POST"
+def path(id):
+    content = request.json
+    if request.method == "GET":
+        graph = db['graphs'].find_one({"id": id})
+    else:
+        return {"success": False}
+    return
 
 @app.route("/node/<int:id>", methods=["POST"])
-def node():
-    pass
+def node(id):
+    content = request.json
+    return db['nodes'].find_one({"id": id})
 
 @app.route("/signup", methods=["POST"])
 def signup():
     content = request.json
-    collection = myDB['users']
-    # myDict = json.loads(content)
+    collection = db['users']
     requesting = []
     requesting.append(InsertOne(content))
     result = collection.bulk_write(requesting)
     return {"success": True}
-
-
-
-@app.route("/<type>/<action>/<int:id>")       #/<type>/<action>/<id>
-def hello(type,action,id):
-    if type == "post":
-        return {"message": "THIS IS A POST"}
-    return {"message": "other"}
-
-
-
-
 
 
 if __name__ == '__main__':
